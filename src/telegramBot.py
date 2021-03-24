130fae350331c8e08a9640174bfe9e1888720d73
Restyled.io <commits@restyled.io>
2021-03-24 23:41:55 +0000

    Restyled by autopep8

--- src/telegramBot.py
+++ src/telegramBot.py
@@ -2,12 +2,14 @@ from telegram.ext import Updater, CommandHandler, CallbackQueryHandler, Callback
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
 import os
 
+
 def main():
     TOKEN = os.getenv('TOKEN')
     updater = Updater(token=TOKEN, use_context=True)
-    updater.dispatcher.add_handler(CommandHandler('start',start))
+    updater.dispatcher.add_handler(CommandHandler('start', start))
     updater.dispatcher.add_handler(CallbackQueryHandler(button))
     updater.start_polling()
+
 
 def start(update, context):
     keyboard = [
@@ -24,19 +26,21 @@ def start(update, context):
     reply_markup = InlineKeyboardMarkup(keyboard)
     update.message.reply_text('Please choose:', reply_markup=reply_markup)
 
+
 def button(update, context):
     days = {
-            "LU": "Lunes",
-            "MA": "Martes",
-            "MI": "Miercoles",
-            "JU": "Jueves",
-            "VI": "Viernes",
-            "SA": "Sabado",
-            "DO": "Domingo",
-            }
+        "LU": "Lunes",
+        "MA": "Martes",
+        "MI": "Miercoles",
+        "JU": "Jueves",
+        "VI": "Viernes",
+        "SA": "Sabado",
+        "DO": "Domingo",
+    }
     query = update.callback_query
     query.answer()
     query.edit_message_text(text=f"El dia seleccionado es: {days[query.data]}")
+
 
 if __name__ == "__main__":
     main()
